ec84ed2601541ea0a022554f2d7140088a5b8e7f
OscarSah <oscar@cybertekschool.com>
2021-09-02 18:36:32 +0300

    Initial commit

---
+++ live_review_sections/week1/Notes.java
@@ -0,0 +1,31 @@
+package week1;
+
+public class Notes {
+	
+	/*
+	 09/12/2021
+		Java Review:
+				Java Intro
+				IDE Tools
+				print statements
+				escape sequences 
+				variables
+				Arithmetic Operators: -, +, *, /, %
+		
+		
+		Jave: programming langauge
+		
+		IDE: Integrated Development Environment
+							Eclipse
+							IntelliJ
+							Visual Studio 
+							Visual Studio Code
+							NetBeans
+		
+		
+		
+
+	 */
+	
+
+}
---
+++ src/day01_hello_world/Main.java
@@ -0,0 +1,13 @@
+package day01_hello_world;
+
+public class Main {
+
+    public static void main(String[] args) {
+    // write your code here
+        System.out.println("Hello World");
+    }
+}
+
+/*
+	Date 
+*/
