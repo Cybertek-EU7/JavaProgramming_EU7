b57743607ecc640b3133c7bca0a5fcd275e03d2c
OscarSah <oscar@cybertekschool.com>
2021-09-10 17:02:23 +0300

    deleted some packages

--- live_review_sections/week1/Notes.java
+++ live_review_sections/week1/Notes.java
@@ -4,23 +4,7 @@ public class Notes {
 	
 	/*
 	 09/12/2021
-		Java Review:
-				Java Intro
-				IDE Tools
-				print statements
-				escape sequences 
-				variables
-				Arithmetic Operators: -, +, *, /, %
-		
-		
-		Jave: programming langauge
-		
-		IDE: Integrated Development Environment
-							Eclipse
-							IntelliJ
-							Visual Studio 
-							Visual Studio Code
-							NetBeans
+	
 		
 		
 		
--- src/day01_hello_world/Main.java
+++ src/day01_hello_world/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
     // write your code here
+
         System.out.println("Hello World");
+       
+      
     }
 }
 
