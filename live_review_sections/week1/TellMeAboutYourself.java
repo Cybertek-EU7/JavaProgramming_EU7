a99faafbd4c308974a5fb6833c1b0333133754ae
OscarSah <oscar@cybertekschool.com>
2021-09-12 21:09:37 +0300

    Class Task is finished

---
+++ live_review_sections/week1/TellMeAboutYourself.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class TellMeAboutYourself {
+
+	public static void main(String[] args) {
+		System.out.print("My name is Oscar\n ");
+		System.out.println("\t I live in Turkiye");
+		System.out.println("I \"teach JAVA\"");
+		System.out.println("My dream job is to become a SDET");
+		
+		
+	}
+}
---
+++ live_review_sections/week1/_Shapes.java
@@ -0,0 +1,22 @@
+package week1;
+
+public class _Shapes {
+
+	// escape squences 
+	//  \t: paragraph space
+	//  \n: append new line
+	// \"  print "
+	//   \\ : prints \
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Make triangle\n---------------------------");
+		System.out.println("\tI want to start my triangle from here");
+		System.out.println("\t     ^");
+		System.out.println("\t    / \\\\");
+		
+	}
+	
+	
+	
+}
