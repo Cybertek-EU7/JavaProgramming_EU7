98c0de04549e279ad83ae34cd620daccc13580bc
OscarSah <oscar@cybertekschool.com>
2021-09-19 21:41:49 +0300

    if and nested if

---
+++ live_review_sections/week2/MonthCalculator.java
@@ -0,0 +1,34 @@
+package week2;
+
+public class MonthCalculator {
+
+	public static void main(String[] args) {
+		// if I give you
+		
+int month = 9;  //how many days in that month  
+
+boolean month28Days = month == 2 ; // false
+boolean month30Days = month == 4 || month == 6 || month == 9 || month == 11; // false
+		
+// Always your code needs to do sanity check		
+		
+	if (month >= 1 && month <= 12 ) {
+		
+		if(month28Days) {
+			System.out.println("28 Days");
+		}else if(month30Days) {
+			System.out.println("30 Days");
+		}else {
+			System.out.println("31 Days");
+		}
+		
+		
+	}else {
+		
+	System.out.println("Invalid Entry");
+	
+	}
+		
+	
+	}
+}
