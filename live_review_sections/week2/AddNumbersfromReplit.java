864c1672551b7766c0db3380fda3044893262c97
OscarSah <oscar@cybertekschool.com>
2021-09-19 22:56:35 +0300

    Update AddNumbersfromReplit.java

--- live_review_sections/week2/AddNumbersfromReplit.java
+++ live_review_sections/week2/AddNumbersfromReplit.java
@@ -6,20 +6,22 @@ public class AddNumbersfromReplit {
 
 	public static void main(String[] args) {
 	    //WRITE YOUR CODE HERE:
-		int num1, num2, num3, sum; // declared the variable
+		int num1, num2, num3; // declared the variable
 
+		double num4, sum;
 		Scanner scan = new Scanner (System.in);
 
-		System.out.println("Enter 3 numbers:");
+		System.out.println("Enter 4 numbers:");
 		num1 = scan.nextInt();
 		num2 = scan.nextInt();
 		num3 = scan.nextInt();
+		num4 = scan.nextDouble();
 
 		// not necessary but you can close that object
 		scan.close();
 		
 		
-		sum = num1 + num2 + num3 ;
+		sum = num1 + num2 + num3 + num4 ;
 
 		System.out.println("Sum of numbers: "+sum);
 
