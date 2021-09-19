9dbffd220cdced84b6d9903a7e97d6fea650756c
OscarSah <oscar@cybertekschool.com>
2021-09-19 19:52:34 +0300

    Create RelationalOperators.java

---
+++ live_review_sections/week2/RelationalOperators.java
@@ -0,0 +1,14 @@
+package week2;
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+		
+		System.out.println(100 > 1000); // false
+		
+		System.out.println('A' == 65); // true
+		
+		System.out.println(15 >= 15);
+		
+	}
+}
