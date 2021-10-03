633aafade54c1ec2c3d8a787f8af1394759bf7d6
OscarSah <oscar@cybertekschool.com>
2021-10-03 18:06:52 +0300

    for loop

---
+++ live_review_sections/week3/BasicLoopExamples.java
@@ -0,0 +1,21 @@
+package week3;
+
+public class BasicLoopExamples {
+
+	public static void main(String[] args) {
+
+		for(int i=0 ; i<10 ; i+=1 )	{
+			System.out.print("Hello  ");
+		}
+
+	//	System.out.println(i); just local to the for loop
+		System.out.println("");
+		System.out.println("--------------------------------------");
+		
+		for(int i=1; i<=10 ; i++) {
+			
+			System.out.println("2 X "+i+" = "+(2*i));
+		}
+	}
+
+}
---
+++ live_review_sections/week3/PrintAllLetters.java
@@ -0,0 +1,12 @@
+package week3;
+
+public class PrintAllLetters {
+
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+
+}
