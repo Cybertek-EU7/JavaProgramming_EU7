d95f888b4f9d10e2ae41faa575fe440b20b6d343
OscarSah <oscar@cybertekschool.com>
2021-10-03 22:29:47 +0300

    Update CustomMethod3.java

--- live_review_sections/week3/CustomMethod3.java
+++ live_review_sections/week3/CustomMethod3.java
@@ -4,6 +4,24 @@ public class CustomMethod3 {
 
 	public static void main(String[] args) {
 		
+		System.out.println(getAgeInDays(10) + " Days old. ");
+		
+		System.out.println(getAgeInDaysWithMSG(10));
+	}
+	
+	
+	public static int getAgeInDays(int years) {
+	
+		 //  int days = years * 365;
+		
+		return years * 365;
+	}
+	
+	
+	public static String getAgeInDaysWithMSG(int years) {
+		
+		return (years * 365) + " Days old.";
+		
 	}
 	
 }
