1918178021a5e03b7508e5a57db4a89441ce0365
OscarSah <oscar@cybertekschool.com>
2021-10-03 19:34:22 +0300

    Update Assignment5_Q_4.java

--- live_review_sections/week3/Assignment5_Q_4.java
+++ live_review_sections/week3/Assignment5_Q_4.java
@@ -1,19 +1,38 @@
 package week3;
 
+import java.util.Scanner;
+
 public class Assignment5_Q_4 {
 	
 	public static void main(String[] args) {
 		
 		/*
 		 *   * Question-4:
-
-         Write a while loop that 
+        Write a while loop that 
          lets the user enter a number. 
          The number should be 
              multiplied by 10, 
              and the result stored in the variable product. 
              The loop should iterate as long as product contains a value less than 100.
 		 */
+
+		// lets the user enter a number
+		Scanner object;  // Declaring a variable named object
+		object = new Scanner(System.in); // initialization of object 
+		
+		int product;  // Declaring a variable
+		product = 0; // initialization 
+		
+		
+		
+		while(product<100) {
+			System.out.println("Enter a number : ");
+			int number = object.nextInt();
+			product+=number*10;
+			System.out.println("Product value is "+ product);
+		}
+		
+		System.out.println("The End");
 		
 		
 		
