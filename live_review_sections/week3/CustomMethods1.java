40c0dda508f1edb9c0d4cf741015c03db4c05854
OscarSah <oscar@cybertekschool.com>
2021-10-03 22:14:39 +0300

    Create CustomMethods1.java

---
+++ live_review_sections/week3/CustomMethods1.java
@@ -0,0 +1,30 @@
+package week3;
+
+public class CustomMethods1 {
+
+	public static void main(String[] args) {
+		
+		int person1 = 10;
+		if (person1>=21) {
+			System.out.println("Eligible to buy");
+		}else {
+			System.out.println("NOT Eligible to buy");
+		}
+		
+		int person2 = 20;
+		if (person2>=21) {
+			System.out.println("Eligible to buy");
+		}else {
+			System.out.println("NOT Eligible to buy");
+		}
+		
+		int person3 = 24;
+		if (person3>=21) {
+			System.out.println("Eligible to buy");
+		}else {
+			System.out.println("NOT Eligible to buy");
+		}
+		
+		
+	}
+}
