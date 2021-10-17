76709edae4f95a2cd6a0bb7c63118e7a2c6a253e
OscarSah <oscar@cybertekschool.com>
2021-10-17 23:02:50 +0300

    Create TestCase.java

---
+++ live_review_sections/week4/TestCase.java
@@ -0,0 +1,14 @@
+package week4;
+
+public class TestCase {
+
+// P.I.Q.: How Do You write a Test Case?
+	
+	String testCaseID;
+	String testCaseDescription;
+	int numberOfTestSteps;
+	boolean isPassed;
+	
+	//  HW : create more attributes and some actions 
+	// create a class with main method and create objects of TestCases
+}
