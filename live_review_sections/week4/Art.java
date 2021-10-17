cd518db654bbb197390af7d63c10963929f166c4
OscarSah <oscar@cybertekschool.com>
2021-10-17 18:38:28 +0300

    Create Art.java

---
+++ live_review_sections/week4/Art.java
@@ -0,0 +1,36 @@
+package week4;
+
+public class Art {
+
+	public static void draw() {
+		System.out.println("Basic drawing");
+	}
+	
+	public static void draw(String color) {
+		System.out.println("Drawing with "+color);
+	}
+	
+	public static void draw(int size) {
+		draw("red");
+		System.out.println("Drawing with size "+size);
+	}
+	
+	public static String draw(String color, int size) {
+		String result = "Drawing with "+color+" and size "+size;
+		return result;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		draw();
+		draw("red");
+		draw(5);
+		
+		System.out.println(draw("yellow",8));
+	}
+	
+	
+
+}
