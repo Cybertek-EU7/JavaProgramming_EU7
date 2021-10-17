78220d6d2b26b97498de4c16a77277cf78b386b2
OscarSah <oscar@cybertekschool.com>
2021-10-17 22:41:52 +0300

    import example

---
+++ live_review_sections/week4/ImportExample.java
@@ -0,0 +1,13 @@
+package week4;
+
+import week3.CustomMethod3;
+
+public class ImportExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println(CustomMethod3.getAgeInDays(35));
+
+	}
+
+}
---
+++ live_review_sections/week4/NextVsNextLine.java
@@ -0,0 +1,16 @@
+package week4;
+
+import java.util.*;
+
+
+public class NextVsNextLine {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+		
+	}
+}
