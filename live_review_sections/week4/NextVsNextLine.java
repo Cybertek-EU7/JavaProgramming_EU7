f52a54fec531f85228abd9daab5f8cbfc8ef8d50
OscarSah <oscar@cybertekschool.com>
2021-10-17 22:54:23 +0300

    Update NextVsNextLine.java

--- live_review_sections/week4/NextVsNextLine.java
+++ live_review_sections/week4/NextVsNextLine.java
@@ -9,6 +9,35 @@ public class NextVsNextLine {
 		
 		Scanner input = new Scanner(System.in);
 		
+		System.out.println("Enter your first Name");
+		
+		String firstName = input.next();
+		
+		System.out.println("Enter your Last Name");
+		String lastName = input.next();
+		
+		System.out.println("Enter your age");
+		short age = input.nextShort();
+		
+		input.nextLine();  // extra for accepting that ENTER button from previous step
+		
+		System.out.println("Enter your addres");
+		String address = input.nextLine();
+		
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(age);
+		System.out.println(address);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
