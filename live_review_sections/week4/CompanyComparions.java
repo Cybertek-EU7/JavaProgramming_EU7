b6a8cbb7e08fe2cdd87f6a66112542e5920abd44
OscarSah <oscar@cybertekschool.com>
2021-10-17 20:02:12 +0300

    createCompany method added

---
+++ live_review_sections/week4/CompanyComparions.java
@@ -0,0 +1,31 @@
+package week4;
+
+public class CompanyComparions {
+
+	public static void main(String[] args) {
+		ScrumTeam company1 = new ScrumTeam();
+		company1.companyName = "Apple";
+		company1.developers = 1000;
+		company1.testers = 100;
+		company1.developerSalary = 200_000;
+		company1.testerSalary = 120_000;
+		company1.companyRevenue = 10_000_000_000d;
+		
+		System.out.println(company1.companyName+ " Company spent "
+		+ company1.costOfTheTeam()+" dollars on the team this year. And their return of Investment is "
+		+ company1.returnOfInvestment());
+	
+		
+		ScrumTeam company2 = new ScrumTeam();
+		
+		company2.createCompany("IBM", 250, 50);
+		
+		System.out.println(company2.companyName);
+		
+	}
+	
+	
+	
+	
+	
+}
--- live_review_sections/week4/ScrumTeam.java
+++ live_review_sections/week4/ScrumTeam.java
@@ -16,4 +16,13 @@ public class ScrumTeam {
 	public double returnOfInvestment() {
 		return companyRevenue/costOfTheTeam();
 	}
+	
+	public void createCompany(String name, int devNums, int tesNums) {  // setter method
+		companyName = name;
+		developers = devNums;
+		testers = tesNums;
+	}
+	
+	
+	
 }
