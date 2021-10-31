4476d0dbd33120359c60db09e06b0aa92b4f76ce
OscarSah <oscar@cybertekschool.com>
2021-10-31 18:11:57 +0300

    Update S03_StringMethodsPractice.java

--- live_review_sections/week5/S03_StringMethodsPractice.java
+++ live_review_sections/week5/S03_StringMethodsPractice.java
@@ -4,10 +4,11 @@ public class S03_StringMethodsPractice {
 	public static void main(String[] args) {
 		
 		String message = "Java is Awesome!";
+					//    0123
 		
 		// length() : takes String as parameter and returns int
 		
-		int count = message.length();
+		int count = message.length();  // count : 16 ---last char is 15 
 		System.out.println("How many characters I have here: "+ count);
 		
 		int sum = 4 + message.length();
@@ -15,6 +16,34 @@ public class S03_StringMethodsPractice {
 		
 		// for iterating through our string object
 		
+		
+		// charAt(): takes an index as parameter and returns me char
+		
+		System.out.println(message.charAt(0)); // J --> first index
+		
+		// what is the last index: length()-1
+		System.out.println(message.charAt(message.length()-1));
+		
+		
+		// contains(): takes a string , and return a boolean
+		// if you want to use one char, you need to turn it into a 
+		// string      (""+char)
+		
+		boolean b = message.contains("Java"); // true
+		
+		boolean b2 = message.contains("J"); // true
+		
+		if (message.contains("Java")) {
+			System.out.println("The message has JAVA");
+		}else {
+			System.out.println("The message does NOT have the word");
+		}
+		
+		if (b2) {
+			System.out.println("The message has J");
+		}else {
+			System.out.println("The message does NOT have");
+		}
 		
 		
 		
