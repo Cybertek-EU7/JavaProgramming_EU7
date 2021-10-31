3eb84873c6f3f667d7bd3fa334a187eb9b5ba50a
OscarSah <oscar@cybertekschool.com>
2021-10-31 23:00:12 +0300

    last example

--- live_review_sections/week5/S09_FrequencyOfWord.java
+++ live_review_sections/week5/S09_FrequencyOfWord.java
@@ -2,13 +2,25 @@ package week5;
 
 public class S09_FrequencyOfWord {
 
+	public static void main(String[] args) {
+		String sentence = "I am an experienced tester. As a tester I can automate browsers."
+				+ " I can test API and DB test";
+		String word = "test";
+		
+		System.out.println(freqOfWord(sentence, word));
+	}
 	
-public static int freqOfWord(String str) {
+	
+public static int freqOfWord(String str, String word) {
 	int count = 0;
 	
-	for (int i = 0; )
+	for (int i = 0; i <= str.length()-word.length();i++) {
+		String each = str.substring(i, i+word.length());
+		if(each.equalsIgnoreCase(word)) {
+			count++;
+		}
 	
-	
+	}
 	
 	return count;
 }
