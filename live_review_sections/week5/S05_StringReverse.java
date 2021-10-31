62708a9cd1d2abd57bc071d56595242137282a98
OscarSah <oscar@cybertekschool.com>
2021-10-31 18:47:29 +0300

    reverse method

--- live_review_sections/week5/S05_StringReverse.java
+++ live_review_sections/week5/S05_StringReverse.java
@@ -11,10 +11,10 @@ public class S05_StringReverse {
 		
 		String reverse = "";
 		
-		reverse +=name.charAt(3); // name.length()-1
-		reverse +=name.charAt(2);
-		reverse +=name.charAt(1);
-		reverse +=name.charAt(0);
+		reverse += name.charAt(3); // name.length()-1
+		reverse += name.charAt(2);
+		reverse += name.charAt(1); // += shorthand operator 
+		reverse += name.charAt(0); // reverse = reverse + name.charAt(0);
 		
 		System.out.println(reverse);
 		
@@ -27,10 +27,25 @@ public class S05_StringReverse {
 		}
 		
 		System.out.println(reverseResult);
+	
+		
+		System.out.println("-----reverse with method--------");
+		
+		String word = "Oscar";
+		System.out.println(reverse(word));
 		
 		
+	}
+	
+	public static String reverse(String str) {
 		
+		String reverseResult = "";
 		
+		for (int i = str.length()-1; i >= 0 ; i--) {
+			reverseResult += str.charAt(i);
+		}
+		
+		return  reverseResult;
 	}
 	
 }
