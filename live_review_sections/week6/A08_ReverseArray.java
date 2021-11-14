904058ff1fabec9668015f4552a35f705c8865a2
OscarSah <oscar@cybertekschool.com>
2021-11-14 22:43:57 +0300

    Update A08_ReverseArray.java

--- live_review_sections/week6/A08_ReverseArray.java
+++ live_review_sections/week6/A08_ReverseArray.java
@@ -1,11 +1,26 @@
 package week6;
 
+import java.util.Arrays;
+
 public class A08_ReverseArray {
 
 	public static void main(String[] args) {
 		
+		int [] nums = {5, 10 , 4, 100}; // {100,4,10,5}
+// one way is creating a second empty array 		
+		int [] reversedNums = new int [nums.length];
+				
+		for (  int i = nums.length -1,  j=0  ;   i >= 0   ; 	i-- , j++         ) 
+		{
+			reversedNums[j] = nums [i];
+			
+		}
+		
+		System.out.println(Arrays.toString(reversedNums));
 		
 		
+		
+//creating a third variable to hold my array variables		
 		
 		
 	}
