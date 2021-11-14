35034eb4efbbb2e72e165ff7fe0b823c2842d02a
OscarSah <oscar@cybertekschool.com>
2021-11-14 23:14:45 +0300

    Update A08_ReverseArray.java

--- live_review_sections/week6/A08_ReverseArray.java
+++ live_review_sections/week6/A08_ReverseArray.java
@@ -21,6 +21,23 @@ public class A08_ReverseArray {
 		
 		
 //creating a third variable to hold my array variables		
+	int x = 5;  // 5
+	int y = 10;  // 100 
+	int temp;
+	// swapping numbers
+	temp = x;
+	x=y;
+	y=temp;
+	System.out.println("x : "+ x + " y "+ y ); 
+	
+	System.out.println("Print before swap: "+ Arrays.toString(nums));
+// int [] nums = {5, 10 , 4, 100};  
+	for (int i = 0; i < nums.length/2 ; i++) {
+		int temp2 = nums[i];
+		nums[i] = nums[nums.length-1-i]; // 4 - 1 - 0 = 3 / 4 - 1 -1 = 2
+		nums[nums.length-1-i] = temp2;
+	}
+	System.out.println("Print after swap: "+ Arrays.toString(nums));
 		
 		
 	}
