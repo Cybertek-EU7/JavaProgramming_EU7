42bc01e3e92f64890bc847b2f41cfad6c60b0365
OscarSah <oscar@cybertekschool.com>
2021-11-14 19:30:31 +0300

    Create A03_ArrayLoopWithConditions.java

---
+++ live_review_sections/week6/A03_ArrayLoopWithConditions.java
@@ -0,0 +1,43 @@
+package week6;
+
+import java.util.Arrays;
+
+public class A03_ArrayLoopWithConditions {
+public static void main(String[] args) {
+	double [] prices = {22.9, 12.4, 43.12, 65.0, 150.9, 100.87, 90.5, 124.9, 234.56};
+	
+	// we put a dummy variable with the same type
+	for (double price : prices) {
+		System.out.println(price);
+	}
+	
+	// first condition: you want to print out prices over 100 
+	System.out.println("----prices over 100------");
+	for (double price : prices) {
+		if (price > 100) {
+		System.out.println(price);
+		}
+	}
+	
+	// second condition: print the prices between 40 - 150 
+	// We have Arrays class that comes with some ready methods to implement
+	 Arrays.sort(prices);
+	// System.out.println(Arrays.toString(prices));
+	System.out.println("----second condition----");
+	for(double each:prices) {
+        if(each>40 && each<150) {
+        System.out.println(each);
+    }}
+	
+	// third condition: count the prices which are more than 60, result count
+	int count = 0;
+	for(double eachPrice : prices) {
+        if(eachPrice>60) {
+        count++;;
+        }
+    }
+
+    System.out.println(count);
+	
+}
+}
