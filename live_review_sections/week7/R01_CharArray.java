32e1e5f4f8c97fae7bfdf8da198f413baecb13e8
OscarSah <oscar@cybertekschool.com>
2021-11-21 18:44:26 +0300

    Anagram Task started

--- live_review_sections/week7/R01_CharArray.java
+++ live_review_sections/week7/R01_CharArray.java
@@ -14,7 +14,7 @@ System.out.println();
 	
 	// what are Strings ? sequence of chars, collection of chars	
 	
-	String word = new String (letters);
+	String word = new String (letters);// it will make more sense when you learn constructors
 	System.out.println(word);
 	
 	String word2 = "silent";
@@ -22,6 +22,14 @@ System.out.println();
 	char [] letters2 = word2.toCharArray();
 	
 	System.out.println(Arrays.toString(letters2));
+	
+	for(char each : word2.toCharArray() ) {
+		System.out.print(each + " ");
+	}
+	
+	
+	
+	
 	
 }
 	
---
+++ live_review_sections/week7/R02_CharArray2.java
@@ -0,0 +1,30 @@
+package week7;
+
+import java.util.Arrays;
+
+public class R02_CharArray2 {
+public static void main(String[] args) {
+	
+	int [] alphabet = new int[26];
+	// 97 is the value of 'a' in ASCII Table
+	
+//	alphabet['l'-97] = alphabet['l'-97]+1;
+//	
+//	alphabet['i'-97] = alphabet['i'-97]+1;
+//	
+//	alphabet['s'-97] = alphabet['s'-97]+1;
+	
+	String word = "listen";
+	for(char each: word.toCharArray()) {
+		alphabet[each-97]=alphabet[each-97]+1;
+	}
+	System.out.println(Arrays.toString(alphabet));
+	
+	String word2 = "filent";
+	for(char each: word2.toCharArray()) {
+		alphabet[each-97]=alphabet[each-97]-1;
+	}
+	System.out.println(Arrays.toString(alphabet));
+	
+}
+}
