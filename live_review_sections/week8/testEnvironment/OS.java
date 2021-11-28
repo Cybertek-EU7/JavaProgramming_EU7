c25185a4967bb931e3f7ee6b41ce43c7f36b2f7e
OscarSah <oscar@cybertekschool.com>
2021-11-28 22:08:12 +0300

    Create OS.java

---
+++ live_review_sections/week8/testEnvironment/OS.java
@@ -0,0 +1,31 @@
+package week8.testEnvironment;
+
+public class OS {
+	
+	private String os;
+	
+	public OS() {
+		
+	}
+
+	public OS(String os) {
+		this.os = os;
+	}
+
+	
+	public String toString() {
+		return "OS [os=" + os + "]";
+	}
+
+	public String getOs() {
+		return os;
+	}
+
+	public void setOs(String os) {
+		this.os = os;
+	}
+	
+	
+	
+
+}
