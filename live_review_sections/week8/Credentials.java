db76d6f300778a9b17693471cb9abe57b313dfb8
OscarSah <oscar@cybertekschool.com>
2021-11-28 21:36:06 +0300

    Update Credentials.java

--- live_review_sections/week8/Credentials.java
+++ live_review_sections/week8/Credentials.java
@@ -1,5 +1,7 @@
 package week8;
 
+
+
 public class Credentials {
 	
 	private String userName = "Spartan300";
@@ -17,13 +19,52 @@ public class Credentials {
 	public String getPassWord() {
 		return passWord;
 	}
-// implement password logic: more than 8 characters, 
-	// at least one Uppercase, lowercase and special
-	
-	
+// implement password logic: more than 8 characters, at least one Uppercase, lowercase and special
 	public void setPassWord(String passWord) {
-		this.passWord = passWord;
+	boolean passwordSize;
+	boolean uppercase = false;
+	boolean lowercase = false;
+	boolean special = false;
+	
+	passwordSize = (passWord.length()>8)? true : false;
+	for(char eachLetter : passWord.toCharArray() ) {
+		if (Character.isUpperCase(eachLetter)) {
+			uppercase = true;
+		}
+		if(Character.isLowerCase(eachLetter)) {
+			lowercase = true;
+		}
+		if(!Character.isLetterOrDigit(eachLetter)) {
+			special = true;
+		}
 	}
+	
+	if(passwordSize&&uppercase&&lowercase&&special) {
+		this.passWord = passWord;
+	}else {
+		this.passWord = null;
+	}
+	
+	
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
