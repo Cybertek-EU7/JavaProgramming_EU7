4055e0b754a3e549295076d20c91251b1ea02880
OscarSah <oscar@cybertekschool.com>
2021-11-28 17:39:33 +0300

    Collections

--- live_review_sections/week8/R02_CollectionsUtility.java
+++ live_review_sections/week8/R02_CollectionsUtility.java
@@ -2,6 +2,7 @@ package week8;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class R02_CollectionsUtility {
 
@@ -12,6 +13,17 @@ public class R02_CollectionsUtility {
 		list.addAll(Arrays.asList('j','a','v','a','i','s','f','u','n'));
 		
 		System.out.println(list.size());
+		
+		System.out.println(list);
+		
+		// we can reverse this arraylist
+		Collections.reverse(list);
+		System.out.println("Reversed list : "+list);
+		
+		// frequency method that helps to look for an element
+		int freOfChar = Collections.frequency(list, 'a');
+		System.out.println(freOfChar);
+		
 
 	}
 
