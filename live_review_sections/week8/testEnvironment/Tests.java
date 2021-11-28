fc61cb0bd87d563aa2f92acf3e4d22c1c6b535a1
OscarSah <oscar@cybertekschool.com>
2021-11-28 22:42:56 +0300

    Tests

---
+++ live_review_sections/week8/testEnvironment/Tests.java
@@ -0,0 +1,36 @@
+package week8.testEnvironment;
+
+import java.util.ArrayList;
+
+public class Tests {
+
+	public static void main(String[] args) {
+		
+		TestEnvironment testOne = new TestEnvironment();
+		
+	//	testOne.browser = "Chrome";
+		
+	testOne.browser = new Browser("Chrome"); // I am calling one parameter constructor to assign browserType
+	testOne.os = new OS("Windows");
+	
+	System.out.println("My test environment for test one is : "+testOne.toString());
+
+	TestEnvironment testTwo = new TestEnvironment();
+	
+
+	testTwo.browser = new Browser();
+	testTwo.browser.setBrowserType("Safari");
+	
+	testTwo.os = new OS();
+	testTwo.os.setOs("MacOs");
+	
+	System.out.println("My test environment for test two is : "+testTwo.toString());
+	
+	
+	
+	ArrayList<TestEnvironment> tests = new ArrayList<>();
+	
+	
+		
+	}
+}
