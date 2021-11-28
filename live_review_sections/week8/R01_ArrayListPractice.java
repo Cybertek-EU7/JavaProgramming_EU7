88ad6a59b497d3702ee73c03471811c580a14618
OscarSah <oscar@cybertekschool.com>
2021-11-28 17:34:25 +0300

    Second Example

--- live_review_sections/week8/R01_ArrayListPractice.java
+++ live_review_sections/week8/R01_ArrayListPractice.java
@@ -13,10 +13,8 @@ public class R01_ArrayListPractice {
 	public static void main(String[] args) {
 		
 	//	System.out.println(listGenerator());
-		
-		
-		
-
+	
+	
 	}
 	
 	
---
+++ live_review_sections/week8/R02_CollectionsUtility.java
@@ -0,0 +1,18 @@
+package week8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class R02_CollectionsUtility {
+
+	public static void main(String[] args) {
+	
+		ArrayList<Character> list = new ArrayList<>();
+		
+		list.addAll(Arrays.asList('j','a','v','a','i','s','f','u','n'));
+		
+		System.out.println(list.size());
+
+	}
+
+}
