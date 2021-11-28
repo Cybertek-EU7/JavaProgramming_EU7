c2d5ac0af4439ba0a4f605a17be3d58952aebbe6
OscarSah <oscar@cybertekschool.com>
2021-11-28 19:45:59 +0300

    Create AmazonTest.java

---
+++ live_review_sections/week8/AmazonTest.java
@@ -0,0 +1,22 @@
+package week8;
+
+import java.util.ArrayList;
+
+public class AmazonTest {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Item> items = new ArrayList<>();
+		
+		items.add(new Item("Sony Vaio i7",950.0,10));
+		items.add(new Item("Lenove", 800.0, 20));
+		items.add(new Item("LG Ultrawide monitor", 450.0, 13));
+		items.add(new Item("HP Laptop",750.0 , 24));
+		
+		
+		
+		
+		
+	}
+
+}
