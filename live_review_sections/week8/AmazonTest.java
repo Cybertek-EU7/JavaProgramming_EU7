754ac130395cd210b5fd8bbba9e9287d577cf81b
OscarSah <oscar@cybertekschool.com>
2021-11-28 19:55:40 +0300

    Update AmazonTest.java

--- live_review_sections/week8/AmazonTest.java
+++ live_review_sections/week8/AmazonTest.java
@@ -1,6 +1,7 @@
 package week8;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class AmazonTest {
 
@@ -13,9 +14,26 @@ public class AmazonTest {
 		items.add(new Item("LG Ultrawide monitor", 450.0, 13));
 		items.add(new Item("HP Laptop",750.0 , 24));
 		
+		// the value of my inventory 
+		double valueOfInventory = 0;
+		// largest quantity of product 
+		int largestQuantity = 0;
+		String nameOfLargest = "";
 		
+		ArrayList<Integer> quantities = new ArrayList<>();
+		for(Item each: items) {
+			valueOfInventory += each.calCost();
+			if(each.quantity>largestQuantity) {
+				largestQuantity=each.quantity;
+				nameOfLargest = each.name;
+			}
+			quantities.add(each.quantity);
+		}
 		
-		
+		System.out.println("Value of inventory :"+valueOfInventory);
+		System.out.println("Item with largest amount : "+nameOfLargest+" and quatity : "+largestQuantity);
+
+	System.out.println(Collections.max(quantities));	
 		
 	}
 
