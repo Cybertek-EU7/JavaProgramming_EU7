769d20e4e86bf50edbeec0e4487fa95a8163d1d8
OscarSah <oscar@cybertekschool.com>
2022-01-02 21:48:33 +0300

    Create UIAutomationTest.java

---
+++ live_review_sections/week12/interface_demo/UIAutomationTest.java
@@ -0,0 +1,30 @@
+package week12.interface_demo;
+
+public class UIAutomationTest {
+
+	public static void main(String[] args) {
+		
+//		WebDriver driver = new ChromeDriver(); // polymorphic way
+	
+// 	WebDriver driver = new WebDriver() ; NOT possible
+	ChromeDriver driver1 = new ChromeDriver();	
+	
+	FirefoxDriver driver2 = new FirefoxDriver();
+	
+	driver1.get("http://practice.cybertekschool.com/");
+	driver1.findElement("//input[@att='value']");
+	System.out.println(driver1.getTitle());
+	driver1.quit();
+	
+	System.out.println("================================================");
+	
+	driver2.get("http://practice.cybertekschool.com/");
+	driver2.findElement("//input[@att='value']");
+	System.out.println(driver2.getTitle());
+	driver2.quit();
+	
+		
+		
+	}
+	
+}
