17992719c168f58b02e610c60d3b5c04f419cb4e
OscarSah <oscar@cybertekschool.com>
2022-01-02 22:43:43 +0300

    Create SuperManTest.java

---
+++ live_review_sections/week12/polymorphism_demo/SuperManTest.java
@@ -0,0 +1,25 @@
+package week12.polymorphism_demo;
+
+public class SuperManTest {
+
+	public static void main(String[] args) {
+		
+		Father spMan1 = new Superman();
+		spMan1.playWithKid();
+		spMan1.feedKid();
+		spMan1.raiseKid();
+		
+		((Superman)spMan1).work("SDET"); // in order to reach methods of Work interface or 
+		// Superman class we need to down-cast
+		
+		Worker spMan2 = new Superman();
+		spMan2.work("QA Engineer");
+		spMan2.getPaid();
+		((Father)spMan2).feedKid();
+		
+		
+		
+		
+		
+	}
+}
