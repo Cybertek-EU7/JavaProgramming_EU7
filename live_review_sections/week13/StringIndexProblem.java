a3e2b9110f74f429436b700ab9fec9575fb7897b
OscarSah <oscar@cybertekschool.com>
2022-01-09 21:36:34 +0300

    Update StringIndexProblem.java

--- live_review_sections/week13/StringIndexProblem.java
+++ live_review_sections/week13/StringIndexProblem.java
@@ -6,7 +6,13 @@ public class StringIndexProblem {
 		
 		String word = "java";
 		
+		try {
 		System.out.println(word.charAt(10));
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("Type of eception: " + e.getClass().getSimpleName());
+			
+		}
 		
 		System.out.println(word.toUpperCase());
 		
