6fd483af614f4fa17b5e887f0fb1e288e653a558
OscarSah <oscar@cybertekschool.com>
2022-01-16 19:54:08 +0300

    Create ListOfMapExample.java

---
+++ live_review_sections/week14/ListOfMapExample.java
@@ -0,0 +1,48 @@
+package week14;
+
+import java.util.*;
+
+public class ListOfMapExample {
+
+	public static void main(String[] args) {
+		
+		Map<String,String> empOneData = new LinkedHashMap<>();
+		empOneData.put("EmpID", "123");
+		empOneData.put("EmpName", "John"); // my name is Earl
+		empOneData.put("JobTitle", "SDET");
+		empOneData.put("Salary","100_000");
+		System.out.println(empOneData);
+		
+		Map<String,String> empTwoData = new LinkedHashMap<>();
+		empTwoData.put("EmpID", "124");
+		empTwoData.put("EmpName", "Mike"); // my name is Earl
+		empTwoData.put("JobTitle", "Developer");
+		empTwoData.put("Salary","120_000");
+		System.out.println(empTwoData);
+		
+		
+	// I want to get Mike's salary
+		System.out.println(empTwoData.get("EmpName")
+				+" gets $"  + empTwoData.get("Salary"));
+		
+	// to put all employees info together with JAVA collections
+		
+		List<Map<String,String>> employeesList = new ArrayList<>();
+		
+		employeesList.add(empOneData);
+		employeesList.add(empTwoData);
+		
+		System.out.println(employeesList);
+		
+		for(Map<String,String> emp : employeesList) {
+			if(emp.get("EmpName").equals("Mike")) {
+				System.out.println(emp.get("Salary"));
+			}
+		}
+		
+		
+		
+	}
+	
+	
+}
